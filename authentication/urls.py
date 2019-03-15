d6f1a3848fd536228c38b6b7e1e362477606fbba
Dan Florea <floread@floread-a01.vmware.com>
2019-03-15 14:39:21 -0700

    Added registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^login/', views.login_user, name="login"),
     url(r'^logout/', views.logout_user, name="logout"),
+    url(r'^register/', views.register_user, name="register"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+
 from django.contrib import messages
 
 def home(request):
@@ -27,3 +30,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You Have Been Logged Out...'))
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You Have Registered...'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    context = {'form':form}
+    return render(request, 'authentication/register.html', context)
